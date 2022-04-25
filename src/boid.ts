150434fed2479f4e245f0f1b40a3fd23bb63545d
권민호 <alsgh458@gmail.com>
2022-04-25 23:24:33 +0900

    feat: cohesion, align in boid

--- src/boid.ts
+++ src/boid.ts
@@ -1,4 +1,5 @@
 import PVector from 'pvectorjs';
+import { NumberLiteralType } from 'typescript';
 
 class Boid {
 
@@ -8,9 +9,10 @@ class Boid {
     r: number;
     maxForce: number;
     maxSpeed: number;
+    neighbordist: number;
 
-    constructor(x: number, y: number){
-
+    constructor(x: number, y: number, neighbordist: number){
+        this.neighbordist = neighbordist;
     }
 
     run(boids: Array<Boid>): void{
@@ -56,7 +58,20 @@ class Boid {
         if (this.position.y > height + this.r) this.position.y = -this.r;
     }
 
-    seek(): PVector {}
+
+    // 목표물에 대한 steering force towards a target
+    // steer = desired minus velocity
+    seek(target: PVector): PVector {
+        let desired: PVector = PVector.prototype.sub(target, this.position);
+
+        // max speed로 조정하다
+        desired.norm();
+        desired.mult(this.maxSpeed);
+
+        let steer: PVector = PVector.prototype.sub(desired, this.velocity);
+        steer.maxMag(this.maxForce);
+        return steer;
+    }
 
 
     applyForce(force: PVector){
@@ -65,8 +80,60 @@ class Boid {
 
     ///
     sperate(boids: Array<Boid>): PVector {}
-    align(boids: Array<Boid>): PVector {}
-    cohesion(boids: Array<Boid>): PVector {}
+
+
+    //정렬하다
+    align(boids: Array<Boid>): PVector {
+        let sum: PVector = new PVector(0, 0);
+        let count: number = 0;
+
+        for(const other of boids){
+            // 1. 개체간의 거리계산
+            const d: number = PVector.prototype.dist(this.position, other.position);
+
+            if ((d > 0) && (d < this.neighbordist)){
+                sum.add(other.velocity);
+                count++;
+            }
+        }
+
+        if(count > 0){
+            sum.div(count);
+            sum.norm();
+            sum.mult(this.maxSpeed);
+            let steer:PVector = PVector.sub(sum, this.velocity);
+            steer.maxMag(this.maxForce);
+            return steer;
+        }else{
+            return new PVector(0, 0);
+        }
+    }
+
+    // For the average position (i.e. center) of 
+    //   all nearby boids, calculate steering vector towards that position
+    cohesion(boids: Array<Boid>): PVector {
+        let sum: PVector = new PVector(0, 0);
+        let count: number = 0;
+
+        for(const other of boids){
+            //1. 개체간의 거리계산
+            const d: number = PVector.prototype.dist(this.position, other.position);
+
+            //2. 개체 간의 거리가 neighbordist보다 작으면, sum 벡터 누적
+            if( (d > 0) && (d < this.neighbordist) ){
+                sum.add(other.position);
+                count++;
+            }
+        }
+
+        if(count > 0){
+            // sum의 평균 구하기
+            sum.div(count);
+            return this.seek(sum);
+        }else{
+            return new PVector(0, 0);
+        }
+    }
     
 
 
